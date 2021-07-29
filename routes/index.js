87f67bdc8148b27dd6ecb323cd71e9a5501cb09b
Dustin Winkler <dustywinkler@gmail.com>
2021-07-29 11:50:44 -0700

    App

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+const format = require('date-fns/formatDistanceToNow')
+
+const router = Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date()
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date()
+  }
+];
+
+/* GET index page. */
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Mini Messageboard',
+    messages,
+    format: require('date-fns/formatDistanceToNow')
+  });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', {
+    title: 'New Message'
+  });
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date()
+  });
+  res.redirect('/');
+});
+
+module.exports = router;
